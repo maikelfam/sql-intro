4fd03c2d41fb9aa36dc7fe02aae2fec009c86540
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-01-16 03:50:50 +0000

    lab attempts

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -24,4 +24,8 @@
 -- | Billy      | Hamilton  |
 -- | Ian        | Happ      |
 
-
+SELECT players.first_name, players.last_name
+FROM stats INNER JOIN players on players.id = stats.player_id
+INNER JOIN teams on teams.id = stats.team_id
+WHERE teams.name = "Chicago Cubs"
+AND teams.year = 2020;
--- lab-4-3.sql
+++ lab-4-3.sql
@@ -6,4 +6,9 @@
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
 
-
+SELECT teams.name, players.first_name, players.last_name, stats.home_runs
+FROM stats INNER JOIN teams on teams.id = stats.team_id
+INNER JOIN players on players.id = stats.player_id
+WHERE teams.year = 2019
+ORDER BY stats.home_runs DESC
+Limit 1;
